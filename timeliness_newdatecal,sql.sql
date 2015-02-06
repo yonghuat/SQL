15d390e733e19d398a0d7b28c0c6a54f2ef07dee
johansew <yonghuat@yahoo.com>
2015-02-06 10:21:22 -0800

    consider year difference

--- timeliness_newdatecal,sql.sql
+++ timeliness_newdatecal,sql.sql
@@ -10,12 +10,14 @@ IS
                  - ( (TRUNC(end_date,'D') - TRUNC(next_day(start_date, 'MONDAY'),'D'))/7*2)
             WHEN to_char(start_date, 'D') NOT IN (7,1) AND to_char(end_date, 'D') IN (7,1)  -- start weekday, end weekend
               THEN
-                trunc(next_day(end_date -6, 'SATURDAY')) - start_date -((TO_CHAR(TRUNC(end_date,'IW'),'W') - TO_CHAR(TRUNC(start_date,'IW'),'W'))*2)
+                trunc(next_day(end_date -6, 'SATURDAY')) - start_date 
+                - ((TRUNC(end_date,'IW') - TRUNC(start_date,'IW'))/7 * 2)
+                
                 -- day difference minus week difference (start monday) * 2 days
             WHEN 
                 to_char(start_date, 'D') IN (7,1) AND to_char(end_date, 'D') IN (7,1)  -- start weekend, end weekend
               THEN
-                  ((TO_CHAR(end_date,'IW') - TO_CHAR(start_date,'IW'))) * 5 --return weekdays in between two days
+                  ((TRUNC(end_date,'IW') - TRUNC(start_date,'IW'))/7 * 5)--return weekdays in between two days
               ELSE 
                 end_date-start_date -  ((TRUNC(end_date,'D')-  trunc(start_date,'D'))/7*2) 
           END,2) INTO diff FROM DUAL;
@@ -23,13 +25,20 @@ IS
   END WdayDiff;
   
 SELECT 
-WdayDiff(TO_DATE('20150209 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150210 11:00:00','YYYYMMDD HH24:MI:SS')) AS a1_weekday_weeday, 
-WdayDiff(TO_DATE('20150202 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150210 11:00:00','YYYYMMDD HH24:MI:SS')) AS a2_weekday_weeday_diff_week, 
-WdayDiff(TO_DATE('20150213 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150215 11:00:00','YYYYMMDD HH24:MI:SS')) AS b1_weekday_weekend, 
-WdayDiff(TO_DATE('20150206 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150215 11:00:00','YYYYMMDD HH24:MI:SS')) AS b2_weekday_weekend_diff_week,
-WdayDiff(TO_DATE('20150214 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150216 11:00:00','YYYYMMDD HH24:MI:SS')) AS c1_weekend_weekday, 
-WdayDiff(TO_DATE('20150207 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150216 11:00:00','YYYYMMDD HH24:MI:SS')) AS c2_weekend_weekday_diff_week,
-WdayDiff(TO_DATE('20150214 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150215 11:00:00','YYYYMMDD HH24:MI:SS')) AS c1_weekend_weekend, 
-WdayDiff(TO_DATE('20150207 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150215 11:00:00','YYYYMMDD HH24:MI:SS')) AS c2_weekend_weekend_diff_week
+WdayDiff(TO_DATE('20150209 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150210 11:00:00','YYYYMMDD HH24:MI:SS')) 
+  AS a1_wkday_wkday, --starts weekday, ends weekday
+WdayDiff(TO_DATE('20141230 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150106 11:00:00','YYYYMMDD HH24:MI:SS')) 
+  AS a2_wkday_wkday_week,  -- starts weekday, ends weekday, different week
+WdayDiff(TO_DATE('20150213 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150215 11:00:00','YYYYMMDD HH24:MI:SS')) 
+  AS b1_wkday_wkend, --starts weekday, ends weekend
+WdayDiff(TO_DATE('20141230 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150110 11:00:00','YYYYMMDD HH24:MI:SS')) 
+  AS b2_wkday_wkendweek, --starts weekday, ends weekend, different week
+WdayDiff(TO_DATE('20150214 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150216 11:00:00','YYYYMMDD HH24:MI:SS')) 
+  AS c1_wkend_wkday,  --starts weekend, ends weekday
+WdayDiff(TO_DATE('20141227 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150102 11:00:00','YYYYMMDD HH24:MI:SS')) 
+  AS c2_wkend_wkday_week, --starts weekend, ends weekday, different week
+WdayDiff(TO_DATE('20150214 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150215 11:00:00','YYYYMMDD HH24:MI:SS')) 
+  AS d1_wkend_wkend,  --starts weekend, ends weekend
+WdayDiff(TO_DATE('20141227 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150111 11:00:00','YYYYMMDD HH24:MI:SS')) 
+  AS d2_wkend_wkend_week --starts weekend, ends weekend, different week
 FROM DUAL;  
-  
