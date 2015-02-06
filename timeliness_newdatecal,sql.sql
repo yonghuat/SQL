409567a3cd4c2aa342cc3ae39d8e2e57e68ddd62
johansew <yonghuat@yahoo.com>
2015-02-06 11:53:45 -0800

    Update timeliness_newdatecal,sql.sql

--- timeliness_newdatecal,sql.sql
+++ timeliness_newdatecal,sql.sql
@@ -42,3 +42,37 @@ WdayDiff(TO_DATE('20150214 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150215 1
 WdayDiff(TO_DATE('20141227 23:00:00','YYYYMMDD HH24:MI:SS'), TO_DATE('20150111 11:00:00','YYYYMMDD HH24:MI:SS')) 
   AS d2_wkend_wkend_week --starts weekend, ends weekend, different week
 FROM DUAL;  
+
+SELECT 
+  assets.workflow_id,
+  asins.sample_id,
+  assets.asset_id,
+  assets.marketplace_id,
+  assets.marketplace_name,
+  assets.studio,
+  assets.gl_product_group_id AS gl_id,
+  assets.gl_product_group_name AS gl_name,
+  assets.category,
+  assets.subcategory,
+  assets.asin,
+  asins.transfer_request, 
+  asins.received AS p2_received, -- received into p2
+  assets.retouch_requested_first,--uploaded for retouch added 1/6/2014
+  assets.publication_pending_first, -- submitted by retouch
+  assets.published_first, -- p2 has received notification that the asset was published to MSA
+  wdaydiff(asins.received, assets.retouch_requested_first) AS studio_cycle_time,
+  wdaydiff(assets.retouch_requested_first, assets.publication_pending_first) AS retouch_cycle_time,
+  wdaydiff(assets.publication_pending_first, assets.published_first) AS publish_cycle_time,
+  wdaydiff(asins.received,assets.published_first) AS imaging_cycle_time
+FROM picture2_ddl.d_photo_studio_assets assets 
+JOIN picture2_ddl.d_photo_studio_asins asins 
+  ON asins.workflow_id = assets.workflow_id
+WHERE 
+asset_type = 'IMAGE_ASSET'
+AND assets.variant IN ('MAIN','GLMR')
+AND assets.source_type_id IN (5400,5405)
+AND assets.erroneous IS NULL
+AND (assets.problem IS NULL OR assets.problem < assets.published_first) 
+AND asins.received IS NOT NULL 
+AND assets.retouch_requested_first IS NOT NULL 
+AND assets.publication_pending_first IS NOT NULL ;
